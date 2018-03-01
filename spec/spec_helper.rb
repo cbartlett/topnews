c7410a4a7a73d8b09f2f9213c7563ef0d464e57c
Mike Riley <wdperson@gmail>
2018-03-01 17:56:14 -0500

    add vcr cassette and config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,9 @@ require 'capybara/rspec'
 require 'vcr'
 
 VCR.configure do |c|
-    c.cassette_library_dir = 'spec/vcr'
-      c.hook_into :webmock
-        c.default_cassette_options = { record: :new_episodes, erb: true }
+  c.cassette_library_dir = 'spec/vcr'
+  c.hook_into :webmock
+  c.default_cassette_options = { record: :new_episodes, erb: true }
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
