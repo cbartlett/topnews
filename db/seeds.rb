01438c92a88a34ce05030ae188c639dd4c822331
Mike Enriquez <mike@enriquez.me>
2022-07-13 09:44:13 -0400

    update_attributes was removed in latest Rails 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-User.find_or_initialize_by(email: 'DonaldGMiller@example.com').update_attributes({
+User.find_or_initialize_by(email: 'DonaldGMiller@example.com').update({
   first_name: 'Donald',
   last_name: 'Miller',
   password: 'eeMaev2shai'
 })
 
-User.find_or_initialize_by(email: 'LawrenceWGrant@example.com').update_attributes({
+User.find_or_initialize_by(email: 'LawrenceWGrant@example.com').update({
   first_name: 'Lawrence',
   last_name: 'Grant',
   password: 'ahR7iecai'
 })
 
-User.find_or_initialize_by(email: 'MargeRWilliams@example.com').update_attributes({
+User.find_or_initialize_by(email: 'MargeRWilliams@example.com').update({
   first_name: 'Marge',
   last_name: 'Williams',
   password: 'Aechugh1ie'
