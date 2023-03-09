af8391b97f9f721d1b011603a3815ec3423fef9c
ilei0893 <ivan@occupier.com>
2023-03-08 19:28:15 -0500

    rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe User do
-  context "creating a new user" do
+  context 'creating a new user' do
     let(:attrs) do
       { first_name: :foo, last_name: :bar, email: 'f@b.c', password: 'foobar123' }
     end
 
-    it "should have first, last, email" do
-      expect { User.create(attrs) }.to change{ User.count }.by(1)
+    it 'should have first, last, email' do
+      expect { User.create(attrs) }.to change { User.count }.by(1)
     end
 
-    it "should require a password" do
+    it 'should require a password' do
       expect(User.new(attrs.except(:password))).to be_invalid
     end
   end
