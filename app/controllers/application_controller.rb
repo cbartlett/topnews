d2d6f762ddc94d26805e62b6679e60c2ceaaf7a9
ilei0893 <ivan@occupier.com>
2023-03-08 20:38:06 -0500

    add authenticate user before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! # Authenticate user on sign in w/ devise
   protect_from_forgery with: :exception
 end
