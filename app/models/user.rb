3a11978a54d8b5bf1af951b8ef91f329d2648708
ilei0893 <ivan@occupier.com>
2023-03-09 23:15:38 -0500

    add fullname helper to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
 
   has_many :favorites, dependent: :destroy
   has_many :posts, through: :favorites
+
+  def full_name
+    [first_name, last_name].compact.join(" ")
+  end
 end
